432fd3d3e13d8611b973a1bdb190ea1e1ff96999
i62cogag <i62cogag@uco.es>
2019-09-25 19:12:10 +0200

    Menu is working

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -1,4 +1,3 @@
-#include <opencv2/opencv.hpp>//change color to grayscale
 #include <opencv2/core/core.hpp> //core routines
 #include <opencv2/imgproc.hpp>//to draw rectangles
 #include <opencv2/core/types.hpp>//to add points
@@ -6,10 +5,11 @@
 
 #include <iostream>
 #include <vector>//to use stl vectors
+#include <cmath>//for pow function
 
 using namespace std;
 
-cv:.Mat image,result;
+cv::Mat image,result;
 void processImg(float c,float b,float g);
 
 // Copy in your program!
@@ -35,27 +35,78 @@ public:
 
 void processImg(float c,float b,float g){
   image.copyTo(result);
+  float pixel = 0;
+  float normalized = 0;
   for(int x = 0; x < result.rows; x++){
     for(int y = 0; y < result.cols; y++){
-      uchar *prt = result.prt<uchar>(y)+3*x;
-      float pixel = (ptr[0] + ptr[1] + ptr[2]) / 255;
+      uchar *ptr = result.ptr<uchar>(y)+3*x;
+      pixel = (ptr[0] + ptr[1] + ptr[2]) / 255;
+      ptr += 3;
+      normalized +=pixel;
     }
   }
+  normalized = c * (pow(normalized,g)) + b;
+  //denormalize the image
+  for(int x = 0; x < result.rows; x++){
+    for(int y = 0; y < result.cols; y++){
+      uchar *ptr = result.ptr<uchar>(y)+3*x;
+      ptr[0] = ptr[1] = ptr[2] = normalized;
+      ptr += 3;
+    }
+  }
+  cv::imshow("Processed",result);
+}
 
 
 //Example of use
 int main(int argc,char **argv){
 
-   CmdLineParser cml(argc,argv);
-   //check if a command is present
-   if (cml["-c"]){
-       cout<<"-c option is in the command line";
-       float f= std::stof( cml("-c"));//convert to float
-       //int i= std::stoi( cml("-p"));//convert to int
-   }
-   //used with default values
-   float f=std::stof(cml("-c","1"));//if -p is not, then, return 1
+  CmdLineParser cml(argc,argv);
+  float c,b,g;
+  //check if a command is present
+  try{
 
+    if(argc < 3){
+      cerr<<"Usage: <input img> <output img>\n Options:\n \t[-i interactive mode] \n \t[-c range: [0,2]] \n \t[-b range: [-1,1]] \n \t[-g range: [0-2]]"<<endl;
+      return 0;
+    }
 
-   processImg();
+    image=cv::imread(argv[1]);
+
+    if( image.rows==0){
+      cerr<<"Error reading image"<<endl;
+      return 0;
+    }
+
+    if (cml["-c"]){
+      c = std::stof( cml("-c"));//convert to float
+      //int i= std::stoi( cml("-p"));//convert to int
+    }else{//used with default values
+      c = std::stof(cml("-c","1"));//if -p is not, then, return 1
+    }
+    if (cml["-b"]){
+      b = std::stof( cml("-b"));//convert to float
+      //int i= std::stoi( cml("-p"));//convert to int
+    }else{  //used with default values
+      b = std::stof(cml("-b","0"));//if -p is not, then, return 1
+    }
+    if (cml["-g"]){
+      g = std::stof( cml("-g"));//convert to float
+      //int i= std::stoi( cml("-p"));//convert to int
+    }else{  //used with default values
+      g = std::stof(cml("-g","1"));//if -p is not, then, return 1
+    }
+
+    cv::namedWindow("image");
+    cv::imshow("image",image);
+
+    cerr<<"c option is in the command line = "<<c<<"\n";
+    cerr<<"b option is in the command line = "<<b<<"\n";
+    cerr<<"g option is in the command line = "<<g<<"\n";
+    //processImg(c,b,g);
+  }
+  catch(const std::exception& ex)
+  {
+    std::cerr << ex.what() << '\n';
+  }
 }
