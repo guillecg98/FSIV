283064d1cb2649814d9db989b71393f29cb40290
i62cogag <i62cogag@uco.es>
2019-09-27 17:05:58 +0200

    Reorganized task 1

---
+++ task-1/build/CMakeFiles/task1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1.dir/task1.cpp.o"
+  "task1.pdb"
+  "task1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
