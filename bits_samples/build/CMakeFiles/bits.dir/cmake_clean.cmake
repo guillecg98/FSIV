06b93f05af09612f8913d56077ed3ca4db7c0d5d
Guille Cosano <i62cogag@uco.es>
2019-11-20 21:17:36 +0100

    bits work

---
+++ bits_samples/build/CMakeFiles/bits.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bits.dir/bits.cpp.o"
+  "bits.pdb"
+  "bits"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bits_samples/build/CMakeFiles/parsing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parsing.dir/parsing.cpp.o"
+  "parsing.pdb"
+  "parsing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parsing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
